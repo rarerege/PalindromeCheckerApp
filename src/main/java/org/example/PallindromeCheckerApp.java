8bd542fd594213f470d1f74a042166d5849801d3
rarerege <raghav.rege@gmail.com>
2026-02-27 14:27:17 +0530

    Base application setup

---
+++ src/main/java/org/example/PallindromeCheckerApp.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class PallindromeCheckerApp {
+    public static void main(String[] args){
+        System.out.print("Welcome to the Palindrome Checker Management System");
+        System.out.print("Version: 1.0");
+        System.out.print("System initialized successfully");
+
+
+
+    }
+}
