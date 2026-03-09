28547ad5071b6118e5d707c8be262608d365a696
rarerege <raghav.rege@gmail.com>
2026-03-09 14:21:01 +0530

    UC8:Linked List Based Palindrome Checker

--- src/main/java/org/example/PallindromeCheckerApp.java
+++ src/main/java/org/example/PallindromeCheckerApp.java
@@ -2,38 +2,84 @@ package org.example;
 
 import java.util.*;
 
+class Node {
+    char data;
+    Node next;
+
+    Node(char data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+class SinglyLinkedList {
+    Node head;
+
+    public void add(char c) {
+        Node newNode = new Node(c);
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+        Node current = head;
+        while (current.next != null) {
+            current = current.next;
+        }
+        current.next = newNode;
+    }
+
+    public Node reverse(Node start) {
+        Node prev = null;
+        Node current = start;
+        Node next = null;
+        while (current != null) {
+            next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+        return prev;
+    }
+
+    public boolean isPalindrome() {
+        if (head == null || head.next == null) {
+            return true;
+        }
+        Node slow = head;
+        Node fast = head;
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        Node secondHalfStart = reverse(slow.next);
+        Node firstHalfStart = head;
+        Node secondHalfCopy = secondHalfStart;
+        boolean palindrome = true;
+        while (secondHalfStart != null) {
+            if (firstHalfStart.data != secondHalfStart.data) {
+                palindrome = false;
+                break;
+            }
+            firstHalfStart = firstHalfStart.next;
+            secondHalfStart = secondHalfStart.next;
+        }
+        slow.next = reverse(secondHalfCopy); // Restore original list
+        return palindrome;
+    }
+}
+
+
 public class PallindromeCheckerApp {
     public static void main(String[] args){
 
-        // Hardcoded input string (you can change this to test)
-        String input = "refer";
+        String input = "level";
 
-        // Normalize input: remove spaces and convert to lowercase
-        String processedInput = input.replaceAll("\\s+", "").toLowerCase();
-
-        // Create a Deque for characters
-        Deque<Character> deque = new ArrayDeque<>();
-
-        // Insert characters into deque
-        for (char ch : processedInput.toCharArray()) {
-            deque.addLast(ch);
+        SinglyLinkedList list = new SinglyLinkedList();
+        for (char c : input.toCharArray()) {
+            list.add(c);
         }
 
-        boolean isPalindrome = true;
-
-        // Remove from front and rear and compare
-        while (deque.size() > 1) {
-            char front = deque.removeFirst();
-            char rear = deque.removeLast();
-
-            if (front != rear) {
-                isPalindrome = false;
-                break;
-            }
-        }
-
-        // Print output in required format
         System.out.println("Input : " + input);
-        System.out.println("Is Palindrome? : " + isPalindrome);
+        System.out.println("Is Palindrome? : " + list.isPalindrome());
     }
 }
